a4334fda7ddb38eccdc1143808a8d8419aa17c4f
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-09-28 20:18:57 +0100

    render Toaste and LocationList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import toast, { Toaster } from "react-hot-toast";
+import LocationList from "./components/LocationList";
 
 function App() {
   return (
     <>
+      <Toaster />
       <Header />
+      <LocationList/>
     </>
   );
 }
