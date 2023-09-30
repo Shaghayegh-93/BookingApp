ef4cb405b23c95a77c4a2374fd7ca319fc5bbd1f
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-09-30 20:32:39 +0100

    add Routes to the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import "./App.css";
 import Header from "./components/Header";
-import toast, { Toaster } from "react-hot-toast";
+import  { Toaster } from "react-hot-toast";
 import LocationList from "./components/LocationList";
+import { Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+import Hotels from "./components/Hotels";
 
 function App() {
   return (
     <>
       <Toaster />
-      <Header />
-      <LocationList/>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<LocationList />} />
+        <Route path="/hotels" element={<AppLayout />}>
+          <Route index element={<Hotels />} />
+          <Route path=":id" element={<p>singleHotels</p>} />
+        </Route>
+      </Routes>
     </>
   );
 }
