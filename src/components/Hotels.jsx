92f4b8ab03d372ea4d35ceb07158d46354e9ad5b
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-10-01 13:09:43 +0100

    move fetching function and relative state to HotelProvider component

--- src/components/Hotels.jsx
+++ src/components/Hotels.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link, useSearchParams } from "react-router-dom";
-import useFetch from "./hooks/useFetch";
 import Loader from "./Loader";
+import useFetch from "./hooks/useFetch";
+import { useHotels } from "./context/HotelProvider";
 
 const Hotels = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const destination = searchParams.get("destination");
-  const room = JSON.parse(searchParams.get("option"))?.room;
-  const { isLoading, data } = useFetch(
-    "http://localhost:5000/hotels",
-    `q=${destination || ""} &accommodates_gte=${room || 1}`
-  );
-  console.log("Destination:", destination);
-  console.log("Room:", room);
+  //   const [searchParams, setSearchParams] = useSearchParams();
+  //   const destination = searchParams.get("destination");
+  //   const room = JSON.parse(searchParams.get("option"))?.room;
+  //   const { isLoading, data } = useFetch(
+  //     "http://localhost:5000/hotels",
+  //     `q=${destination || ""} &accommodates_gte=${room || 1}`
+  //   );
+  const { isLoading, data } = useHotels();
+
   if (isLoading) <Loader />;
 
   return (
