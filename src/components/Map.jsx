568868cc576a2916b899ecf28dbcb5f655fe26cb
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-10-01 21:43:39 +0100

    add Map component

---
+++ src/components/Map.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import { useHotels } from "./context/HotelProvider";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { useSearchParams } from "react-router-dom";
+import useGeoLocation from "./hooks/useGeoLocation";
+
+const Map = () => {
+  const { data } = useHotels();
+  const [mapCenter, setMapCenter] = useState([50, 1]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  const { isLoadingGeoPosition, geoLocationPosition, error, getPosition } = useGeoLocation();
+   
+  console.log("isLodingGeoPosition:", isLoadingGeoPosition);
+
+  useEffect(() => {
+    if (lat && lng) setMapCenter([lat, lng]);
+  }, [lat, lng]);
+
+  useEffect(() => {
+    if (geoLocationPosition?.lat && geoLocationPosition?.lng)
+      setMapCenter([geoLocationPosition.lat, geoLocationPosition.lng]);
+  }, [geoLocationPosition]);
+
+  return (
+    <div className="flex-1 bg-tex100 relative">
+      <MapContainer
+        className="h-full"
+        center={[lat || 51, lng || 3]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <button
+          onClick={getPosition}
+          className="py-1 px-2 text-xs font-bold rounded-lg absolute bottom-2 left-2 bg-primar600 text-white  z-[1000] shadow-[0_0_10px_rgba(0,0,0,0.3)]"
+        >
+          {isLoadingGeoPosition ? "Loading" : " Use Your Location"}
+        </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <ChangeCenter position={mapCenter} />
+        {data.map((item) => (
+          <Marker key={item.id} position={[item.latitude, item.longitude]}>
+            <Popup>{item.host_location}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
