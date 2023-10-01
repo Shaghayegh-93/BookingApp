0f5ccaa05fba29f08d8f02f0b4fbc22ba3dcb3bb
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-10-01 13:06:05 +0100

    import Map component

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Map from "./Map";
 
 const AppLayout = () => {
   return (
     <div className="mt-4 flex justify-between  items-stretch h-[calc(100vh-130px)] ">
       <div className="w-2/4 md:w-[35%] overflow-y-scroll pr-4">
-        
         <Outlet />
       </div>
-      <div className="flex-1 bg-tex100 relative">map</div>
+      <Map/>
     </div>
   );
 };
