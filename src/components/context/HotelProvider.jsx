ac3872dc5cda9fce75d2ed0c5226a9c805b968bc
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-10-03 21:56:27 +0100

    add state and fubction for getting single hotel data

--- src/components/context/HotelProvider.jsx
+++ src/components/context/HotelProvider.jsx
@@ -1,20 +1,38 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const HotelContext = createContext();
+const BASE_URL = "http://localhost:5000/hotels";
 
 function HotelProvider({ children }) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [isLoadingCurrentHotel, setIsLoadingCurrentHotel] = useState(false);
+  const [currentHotel, setCurrentHotel] = useState(null);
   const destination = searchParams.get("destination");
   const room = JSON.parse(searchParams.get("option"))?.room;
   const { isLoading, data } = useFetch(
-    "http://localhost:5000/hotels",
+    BASE_URL,
     `q=${destination || ""} &accommodates_gte=${room || 1}`
   );
 
+  async function getSingleHotel(id) {
+    setIsLoadingCurrentHotel(true);
+    try {
+      const { data } = await axios.get(`${BASE_URL}/${id}`);
+      setCurrentHotel(data);
+      setIsLoadingCurrentHotel(false);
+    } catch (error) {
+      toast.error(error.message);
+      setIsLoadingCurrentHotel(false);
+    }
+  
+  }
+
   return (
-    <HotelContext.Provider value={{ isLoading, data }}>
+    <HotelContext.Provider value={{ isLoading, data,isLoadingCurrentHotel,currentHotel,getSingleHotel }}>
       {children}
     </HotelContext.Provider>
   );
@@ -25,4 +43,3 @@ export default HotelProvider;
 export function useHotels() {
   return useContext(HotelContext);
 }
-
