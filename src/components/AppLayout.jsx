0cee453d3d48008cd7e668b06cbc7c537fc8ff4f
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-10-05 21:12:45 +0100

    make a Map component dynamic by pass data as proprty

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Map from "./Map";
+import { useHotels } from "./context/HotelProvider";
 
 const AppLayout = () => {
+  const { data } = useHotels();
   return (
     <div className="mt-4 flex justify-between  items-stretch h-[calc(100vh-130px)] ">
       <div className="w-2/4 md:w-[35%] overflow-y-scroll pr-4">
         <Outlet />
       </div>
-      <Map/>
+      <Map  markedLocation={data}/>
     </div>
   );
 };
