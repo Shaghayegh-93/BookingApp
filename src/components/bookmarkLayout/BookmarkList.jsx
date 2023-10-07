c75cfafb3c7e26840835625faa65d53fc5a8c0db
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-10-07 23:34:05 +0100

    improve the logic of component

--- src/components/bookmarkLayout/BookmarkList.jsx
+++ src/components/bookmarkLayout/BookmarkList.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useBookmarkList } from "../context/BookmarkListProvider";
+import Loader from "../Loader";
+import { Link } from "react-router-dom";
+import ReactCountryFlag from "react-country-flag";
 
 const BookmarkList = () => {
+  const { data, isLoading, currentBookmarkList } = useBookmarkList();
+  if (isLoading) return <Loader />;
   return (
-    <div>BookmarkList</div>
-  )
-}
+    <div>
+      <h2>BookmarkList</h2>
+      <div className="mt-4">
+        {data.map((item) => {
+          return (
+            <Link
+              key={item.id}
+              to={`${item.id}?lat=${item.latitude}&lng=${item.longitude}`}
+            >
+              <div
+                className={`mb-4 border border-solid border-tex400 rounded-2xl p-4 flex flex-col md:flex-row items-center justify-between ${
+                  item.id === currentBookmarkList?.id
+                    ? "border border-primar600 bg-tex100"
+                    : ""
+                }`}
+              >
+                <ReactCountryFlag countryCode={item.countryCode} svg /> &nbsp;
+                <strong> {item.cityName}</strong> &nbsp;
+                <span>{item.country}</span>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default BookmarkList
+export default BookmarkList;
