116370bf318e7a4a4f6d98067bf31f804d72eeb5
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-10-10 21:41:13 +0100

    add clickHandler function and trash icon

--- src/components/bookmarkLayout/BookmarkList.jsx
+++ src/components/bookmarkLayout/BookmarkList.jsx
@@ -3,15 +3,23 @@ import { useBookmarkList } from "../context/BookmarkListProvider";
 import Loader from "../Loader";
 import { Link } from "react-router-dom";
 import ReactCountryFlag from "react-country-flag";
+import { TrashIcon } from "@heroicons/react/24/solid";
 
 const BookmarkList = () => {
-  const { data, isLoading, currentBookmarkList } = useBookmarkList();
+  const { bookmarks, isLoading, currentBookmarkList, deleteBookmark } =
+    useBookmarkList();
+
+  const removeHandler = async (e, id) => {
+    e.preventDefault();
+    await deleteBookmark(id);
+  };
   if (isLoading) return <Loader />;
+  if(!bookmarks.length) return <p>There is no bookmarked location.</p>
   return (
     <div>
       <h2>BookmarkList</h2>
       <div className="mt-4">
-        {data.map((item) => {
+        {bookmarks.map((item) => {
           return (
             <Link
               key={item.id}
@@ -24,9 +32,14 @@ const BookmarkList = () => {
                     : ""
                 }`}
               >
-                <ReactCountryFlag countryCode={item.countryCode} svg /> &nbsp;
-                <strong> {item.cityName}</strong> &nbsp;
-                <span>{item.country}</span>
+                <div className="flex flex-col items-center md:block ">
+                  <ReactCountryFlag countryCode={item.countryCode} svg /> &nbsp;
+                  <strong> {item.cityName}</strong> &nbsp;
+                  <span>{item.country}</span>
+                </div>
+                <button onClick={(e) => removeHandler(e, item.id)}>
+                  <TrashIcon className="text-rose-500 w-4 h-4 mt-4 md:mt-0" />
+                </button>
               </div>
             </Link>
           );
