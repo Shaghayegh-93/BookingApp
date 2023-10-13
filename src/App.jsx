8fa0145a57edad392e5aa1b3d125815b5ae66941
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-10-13 22:58:36 +0100

    import ProtectedRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import BookmarkListProvider from "./components/context/BookmarkListProvider";
 import SingleBookmark from "./components/bookmarkLayout/SingleBookmark";
 import Login from "./components/Login";
 import AuthProvider from "./components/context/AuthProvider";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
